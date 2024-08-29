992f00daf3168158af58e7ac7d12ea948ef6a489
KartikPat250905 <kartikpat25@gmail.com>
2024-08-29 10:45:40 +0300

    map rendering added without features

--- drakula/__main__.py
+++ drakula/__main__.py
@@ -10,3 +10,16 @@ if __name__ == '__main__':
     game = GameDatabaseFacade(db)
     airports = game.fetch_random_airports(5, "OC")
     pprint(airports)
+
+    pygame.init()
+    screen = pygame.display.set_mode((1280, 644))
+    map = pygame.image.load("map.jpg")
+
+    running = True
+    while running:
+        screen.fill((255, 255, 255))
+        screen.blit(map,(0,0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        pygame.display.update()

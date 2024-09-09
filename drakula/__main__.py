dcacfbde556651d770b3c10acf90d38c0e36db42
ktnlvr <artur.roos.artur@gmail.com>
2024-09-09 15:56:01 +0300

    Add a logger

--- drakula/__main__.py
+++ drakula/__main__.py
@@ -1,22 +1,27 @@
-from math import pi, tau
+from logging import basicConfig as config_logging, INFO
 
 import dotenv
 import pygame
 
 from .game import MapScene
+from .debug import debug_layers
 from .db import Database, GameDatabaseFacade
 from .state import GameState
 from .renderer import Renderer
+from .logging import logger
 
-GRAPH_PRUNE_LEN = 10
 
 def main(*args, **kwargs):
+    logger.info("Setting up the database")
+
     db = Database()
     game = GameDatabaseFacade(db)
 
+    logger.info("Fetching airports")
     airports = list()
     for continent in game.continents:
         airports.extend(game.fetch_random_airports(4, continent))
+    logger.info(f"Got {len(airports)} airports")
 
     state = GameState(airports)
 
@@ -42,6 +47,10 @@ def main(*args, **kwargs):
         renderer.end()
         scene = scene.next_scene
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     dotenv.load_dotenv()
+    config_logging(level=INFO)
+    if layers := debug_layers():
+        logger.info(f"{len(layers)} debug layers enabled: {', '.join((layer.lower() for layer in layers))}")
     main()

7742e711daa919189a39a251c2a7a082d5bbdd54
Kartik Patel <166979981+KartikPat250905@users.noreply.github.com>
2024-10-04 21:42:12 +0300

    Added documentation for dracula.py

--- drakula/dracula.py
+++ drakula/dracula.py
@@ -2,10 +2,24 @@ from .state import GameState, AirportStatus
 
 
 class DraculaBrain:
+    """
+    A class having the logic behind the movement of the dracula
+
+    Attributes:
+    ----------
+    visited: set
+    An object of class set containing the moves the dracula has made so far.
+    """
     def __init__(self):
         self.visited = set()
 
     def list_moves(self, state: GameState, location: int) -> list[tuple[float, int]]:
+        """
+        :param state: Takes the current state of the game
+        :param location: The current index of the position of dracula in airport's list
+        :return: A list of tuples in which elements consist of weight(float)
+        and the index of the neighbouring airport
+        """
         weighted = []
         neighbours = state.graph[location]
         for neighbour in neighbours:
